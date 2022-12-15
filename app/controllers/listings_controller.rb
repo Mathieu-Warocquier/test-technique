266f4cf05c36ffce653e7a1c0dc6f3e7a0335a8a
Mathieu Warocquier <mathieu.warocquier@gmail.com>
2022-12-15 11:22:35 +0100

    controller listing

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,45 @@
 class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+
+  def create
+      @list = List.new(list_params)
+      if @list.save
+        redirect_to list_path(@list)
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+  def edit
+  end
+
+  def update
+    @listing.update(params[:listing])
+    redirect_to listing_path(@listing)
+  end
+
+  def destroy
+    @listing.destroy
+    redirect_to listings_path, status: :see_other
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:num_rooms)
+  end
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
 end
